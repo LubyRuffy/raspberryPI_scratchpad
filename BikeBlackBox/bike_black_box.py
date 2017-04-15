d917f7b6d3da93c102e58d47515606b905f399ce
Marcin Zalewski <zalkap@gmail.com>
2017-04-15 12:49:43 +0100

    bike_black_box logger changed

--- BikeBlackBox/bike_black_box.py
+++ BikeBlackBox/bike_black_box.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     logger.info(gps_message) 
     
     curr_time = datetime.datetime.now()
-    if (curr_time.second == 0 or curr_time.second < start_time.second)
+    if (curr_time.second == 0 or curr_time.second < start_time.second)\
         and abs(curr_time.minute - start_time.minute) % 10 == LOG_FILE_CADENCE:
       
       log_file      = "BBB.{0}.log".format(datetime.datetime.now().strftime("%Y%m%d_%H:%M"))
