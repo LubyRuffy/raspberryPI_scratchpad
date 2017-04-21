473c07427584b650650701816ae11c4b3abba54c
Marcin Zalewski <zalkap@gmail.com>
2017-04-21 22:02:52 +0100

    speed calculation added

--- MBB/gps_reader.py
+++ MBB/gps_reader.py
@@ -2,6 +2,8 @@
 
 from __future__   import division
 from serial       import Serial
+from math         import cos, acos, sin, pi
+
 import datetime
 
 
@@ -13,8 +15,9 @@ import datetime
 
 class GPSreader():
   
-  __KNOT = 1.852
-    
+  __KNOT  = 1.852
+  __r     = 6371000
+
   _GPRMC_pattern  = { 'HOUR':1, 'STATUS':2, 'LAT':3, 'LAT_NS':4, 'LON':5, 'LON_EW':6, 'SPEED':7,
                       'COURSE':8, 'DATE':9,  'VAR':10, 'VAR_E_W':11, 'CHKS':12, 'LENGTH':13}
   
@@ -27,9 +30,16 @@ class GPSreader():
     else:
       self.get_coords = None
         
-    self._speed       = 0
-    self._latitude    = 0
-    self._longitude   = 0
+    self._speed           = 0
+    self._speed_calc      = 0
+    self._latitude        = 0
+    self._longitude       = 0
+    self._latitude_prev   = 0
+    self._longitude_prev  = 0
+    
+    self._time_now        = 0
+    self._time_prev       = 0
+    
     self._timestamp   = ""
     self._gps_raw     = {}
     
@@ -109,17 +119,53 @@ class GPSreader():
     except:
       return -1
       
+      
+  def _calc_speed(self):
+
+		lat1 = (self._latitude_prev   * pi) / 180
+		lon1 = (self._longitude_prev  * pi) / 180
+		
+		lat2 = (self._latitude  * pi) / 180
+		lon2 = (self._longitude * pi) / 180
+
+		rho1	= self.__r * cos(lat1)
+		z1		= self.__r * sin(lat1)
+		x1		= rho1 * cos(lon1)
+		y1		= rho1 * sin(lon1)
+		
+		rho2	= self.__r * cos(lat2)
+		z2		= self.__r * sin(lat2)
+		x2		= rho2 * cos(lon2)
+		y2		= rho2 * sin(lon2)
+		
+		dot			= x1 * x2 + y1 * y2 + z1 * z2
+		cos_theta	= dot / (self.__r ** 2)
+
+		theta = acos(round(cos_theta, 6))
+
+		distance = self.__r * theta
+		
+		speed_calc	= (distance / (self._time_prev - self._time_now)) * 3.6 
+		
+		return speed_calc
+      
           
   def get_coords(self, gps_str):
     
-    coords_dict = {'LAT':0, 'LON':0, 'SPEED':0, 'TIME':""}
+    coords_dict = {'LAT':0, 'LON':0, 'GPS_SPEED':0, 'CALC_SPEED':0, 'TIME':""}
     
     self._split_gps_str(gps_str)
     if self._gps_raw['STATUS'] == 'A':
-      self._latitude  = self._gps2coords(self._gps_raw['LAT'], self._gps_raw['LAT_NS'])
-      self._longitude = self._gps2coords(self._gps_raw['LON'], self._gps_raw['LON_EW'])
-      self._speed     = self._gps2speed(self._gps_raw['SPEED'])
-      self._timestamp = self._gps2time(self._gps_raw['HOUR'], self._gps_raw['DATE'])
+      
+      self._time_now    = time.time()
+      self._latitude    = self._gps2coords(self._gps_raw['LAT'], self._gps_raw['LAT_NS'])
+      self._longitude   = self._gps2coords(self._gps_raw['LON'], self._gps_raw['LON_EW'])
+      self._speed       = self._gps2speed(self._gps_raw['SPEED'])
+      self._timestamp   = self._gps2time(self._gps_raw['HOUR'], self._gps_raw['DATE'])
+      self._speed_calc  = self._calc_speed()
+      
+      self._latitude_prev, self._longitude_prev = self._latitude, self._longitude
+      self._time_prev = self._time_now
       
       coords_dict     = { 'LAT':self.latitude, 'LON':self.longitude,
                           'SPEED':self.speed, 'TIME':self.timestamp}
