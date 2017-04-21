776bbec4447b441d6a2650b95bd9a5f9c80c75b5
Marcin Zalewski <zalkap@gmail.com>
2017-04-21 23:24:10 +0100

    gyroscope test

--- MBB/mpu_test.py
+++ MBB/mpu_test.py
@@ -6,6 +6,7 @@ import mpu_6050, sys, time
 mpu = mpu_6050.MPU6050()
 
 while True:
-  sys.stdout.write(str(mpu.readSensors()) + "\r")
+  mpu_dict = mpu.readSensors()
+  sys.stdout.write(mpu_dict['GX'] + "\r")
   time.sleep(0.1)
   sys.stdout.flush()
