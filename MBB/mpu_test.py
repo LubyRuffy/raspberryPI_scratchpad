9b098bca31fbaaacf7af5e451ed6af2e46299668
Marcin Zalewski <zalkap@gmail.com>
2017-04-21 23:45:23 +0100

    gyroscope test

--- MBB/mpu_test.py
+++ MBB/mpu_test.py
@@ -7,6 +7,10 @@ mpu = mpu_6050.MPU6050()
 
 while True:
   mpu_dict = mpu.readSensors()
-  sys.stdout.write(str(mpu_dict['AX']) + "\r")
-  time.sleep(0.1)
+  mpu_str = "AX: {:<0}, AY: {:<1}, AZ: {:<2}, GX: {:<3}, GY: {:<4}, GZ: {:<5} \r"
+  sys.stdout.write(mpu_str.format(mpu_dict['AX'], mpu_dict['AY'], mpu_dict['AZ'],
+                                  mpu_dict['GX'], mpu_dict['GY'], mpu_dict['GZ']))
   sys.stdout.flush()
+  time.sleep(0.1)
+  
+  
