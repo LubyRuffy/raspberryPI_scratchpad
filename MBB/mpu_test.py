de96c7b11edc3d4b31a0f259addd7663d6652e11
Marcin Zalewski <zalkap@gmail.com>
2017-04-21 23:20:55 +0100

    gyroscope test

--- MBB/mpu_test.py
+++ MBB/mpu_test.py
@@ -1,10 +1,10 @@
 
 
-import mpu_6050, sys 
+import mpu_6050, sys, time 
 
 
-mpu = MPU6050()
+mpu = mpu_6050.MPU6050()
 
 while True:
   sys.stdout.write(str(mpu.readSensors()) + "\r")
-   
+  time.sleep(0.1)
